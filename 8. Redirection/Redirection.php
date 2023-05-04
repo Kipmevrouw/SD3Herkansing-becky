445603d2d1641d63f5936e39fc6e70eb3b707cd4
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2023-05-04 13:50:57 +0200

    8. Redirection en uitleg

---
+++ 8. Redirection/Redirection.php
@@ -0,0 +1,9 @@
+<?php
+header("Location: https://www.beckyvandermeulen.nl/voorbeeld.php");
+exit;
+
+// In dit voorbeeld wordt de "header" functie gebruikt om een 
+// HTTP-koptekst te verzenden met de "Location" parameter ingesteld 
+// op de URL van de nieuwe pagina. De "exit" functie wordt vervolgens 
+// gebruikt om te voorkomen dat de rest van de PHP-code wordt uitgevoerd.
+?>
